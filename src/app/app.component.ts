9c4a4361cae87d34ddb7ebc90b3ca1d7b953775d
Aman Kumar <aman.kumar@quovantis.com>
2020-05-12 00:10:55 +0530

    updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ElonMuskNameGenerator';
+  public first: string;
+  public middle: string;
+  public last: string;
+  public doAnimate: boolean;
+
+  private _alphabet = ['q', 'w', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+  private _latinChars = ['ä', 'å', 'é', 'þ', 'ü', 'ú', 'í', 'ó', 'ö', 'á', 'ß', 'ð', 'ø', '¶', 'æ', '©', 'ñ', 'µ', 'ç', '¿', '®'];
+  private _specialChars = ['_', '~', '|', '^', '/', '\\', '-', '<', '>', '?', ':', ';', '=', '+', '-', '%', '#', '@', '!'];
+
+  constructor() {
+    this.buildName();
+  }
+
+  buildName = () => {
+    this.doAnimate = false;
+    this.first = this._alphabet[this.getRandomInt(1, this._alphabet.length - 1)].toUpperCase();
+    this.middle = this._latinChars[this.getRandomInt(1, this._latinChars.length - 1)].toUpperCase();
+
+    const x = this._alphabet[this.getRandomInt(1, this._alphabet.length - 1)];
+    const y = this._specialChars[this.getRandomInt(1, this._specialChars.length - 1)];
+    const z = this.getRandomInt(1, 99);
+
+    this.last = (x + y + z).toUpperCase();
+
+    setTimeout(() => {
+      this.doAnimate = true;
+    }, 0);
+  }
+
+  getRandomInt = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+
 }
